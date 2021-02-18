d09a88523f25f269e765a5c3679a8a9ff7c7bc4f
gunasourcehub <78465794+gunasourcehub@users.noreply.github.com>
2021-02-18 13:31:04 +0530

    Initial commit

---
+++ database-h2-memory-app/src/main/java/com/example/databaseh2memoryapp/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.example.databaseh2memoryapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.databaseh2memoryapp.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
---
+++ database-h2-memory-app/src/test/java/com/example/databaseh2memoryapp/DatabaseH2MemoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.databaseh2memoryapp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DatabaseH2MemoryAppApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
